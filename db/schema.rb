3abdee50c7da1f638e427760bb36af40a59019b8
Kohei Kudo <koheikudo1009@yahoo.co.jp>
2018-12-24 07:39:37 +0000

    init

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181216043214) do
+ActiveRecord::Schema.define(version: 20181224073832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,12 +34,12 @@ ActiveRecord::Schema.define(version: 20181216043214) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
     t.string "company"
     t.string "industry"
     t.string "matter"
     t.string "episode"
     t.string "period"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
