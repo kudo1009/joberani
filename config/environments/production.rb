e988e1740125f39a1db601cc8e8f0baf824589f3
Kohei Kudo <koheikudo1009@yahoo.co.jp>
2018-12-24 05:05:46 +0000

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'stormy-scrubland-56809.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'rocky-mesa-16933.herokuapp.com/' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
